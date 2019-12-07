12b6180cbd227a01e975eeb7ae3f0d0113b15007
LiYanPeng <xp970805@gmail.com>
2019-12-07 16:00:45 +0800

    first commit

---
+++ src/views/index.js
@@ -0,0 +1,5 @@
+export { default as Account } from './Account';
+export { default as Dashboard } from './Dashboard';
+export { default as NotFound } from './NotFound';
+export { default as SignIn } from './SignIn';
+export { default as SignUp } from './SignUp';
