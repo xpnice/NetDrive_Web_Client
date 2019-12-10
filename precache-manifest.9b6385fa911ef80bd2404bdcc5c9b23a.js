95e589bb2f6e66526428d3542ffd928c3ca04786
LiYanPeng <xp970805@gmail.com>
2019-12-10 15:21:29 +0800

    Updates

---
+++ precache-manifest.9b6385fa911ef80bd2404bdcc5c9b23a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab6897a69896ba3d025ff9c3d6243a7",
+    "url": "/Net_Drive_Web_Client/index.html"
+  },
+  {
+    "revision": "22f5248c7cb66f60eb15",
+    "url": "/Net_Drive_Web_Client/static/css/2.222cfc28.chunk.css"
+  },
+  {
+    "revision": "aae7903a1c14c24172e2",
+    "url": "/Net_Drive_Web_Client/static/css/main.c99bf7f0.chunk.css"
+  },
+  {
+    "revision": "22f5248c7cb66f60eb15",
+    "url": "/Net_Drive_Web_Client/static/js/2.c25e220c.chunk.js"
+  },
+  {
+    "revision": "aae7903a1c14c24172e2",
+    "url": "/Net_Drive_Web_Client/static/js/main.841bbca8.chunk.js"
+  },
+  {
+    "revision": "3bc811d8206fb9ddc571",
+    "url": "/Net_Drive_Web_Client/static/js/runtime~main.d34c10e8.js"
+  }
+]);
